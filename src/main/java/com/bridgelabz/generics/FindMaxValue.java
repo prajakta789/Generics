2b726ca7929893ce3cbb09dc076534f71ae6c898
prajakta <prajaktamahajan789@gmail.com>
2021-10-18 01:34:42 +0530

    Added:Refactor all three to one genric method

--- src/main/java/com/bridgelabz/generics/FindMaxValue.java
+++ src/main/java/com/bridgelabz/generics/FindMaxValue.java
@@ -1,36 +1,21 @@
 package com.bridgelabz.generics;
-import java.util.Arrays;
- 
 
-public class FindMaxValue<T extends Comparable<T>> {
-    T[] elements;
 
-    public FindMaxValue(T[] elements) {
-        this.elements = elements;
-    }
+public class FindMaxValue{
 
-    /**
-     * Generic method to store values in array and return maximum value.
-     * We can store any size parameter
-     * @param elements : Stores the value  in the array and compare it
-     * @return max : Maximum of three values
-     */
+	public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+		T max = a1;
+		if (a2.compareTo(max) > 0)
+			max = a2;
+		if (a3.compareTo(max) > 0)
+			max = a3;
+		return max;
+	}
 
-    public static <T extends Comparable<T>> T maxOfValues(T[] elements) {
-        Arrays.sort(elements);
-        int lenght = elements.length;
-        T max = elements[lenght - 1];
-        System.out.printf("\nThe Maximum value in all of them is : %s ", max);
-        return max;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome To Find Maximun Problem Using Generics");
-        Integer[] intMax = { 10, 24, 68, 80, 42, 46, 12, 58 };
-        maxOfValues(intMax);
-        Float[] floatMax = { 1.4f, 87.3f, 81.8f, 40.2f, 4.6f };
-        maxOfValues(floatMax);
-        String[] stringMax = { "ABC", "IJK", "PQR", "BCD", "MNO", "LMN", "RST", "XYZ" };
-        maxOfValues(stringMax);
-    }
-}
+	public static void main(String[] args) {
+		System.out.println("Welcome To Find Maximun Problem Using Generics \n");
+		System.out.println("The maximum value between the three integer is : " + maxOfValues(3, 8, 5)+ "\n");
+		System.out.println("The maximum value between the three float is : " + maxOfValues(1.5f, 3.56f, 5.87f) + "\n");
+		System.out.println("The maximum value between the three string is : " + maxOfValues("abc", "pqr", "xyz"));
+	}
+} 
