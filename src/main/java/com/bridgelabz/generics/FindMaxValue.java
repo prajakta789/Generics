f403df1f2a58405d37d1f16c3e9c634ac10ae8f0
prajakta <prajaktamahajan789@gmail.com>
2021-10-18 01:21:45 +0530

    Added:Extend the max method to take more than three prameters

--- src/main/java/com/bridgelabz/generics/FindMaxValue.java
+++ src/main/java/com/bridgelabz/generics/FindMaxValue.java
@@ -1,55 +1,35 @@
 package com.bridgelabz.generics;
+import java.util.Arrays;
+public class FindMaxValue<T extends Comparable<T>> {
 
-public class FindMaxValue {
+    T[] elements;
 
+    public FindMaxValue(T[] elements) {
+        this.elements = elements;
+    }
 
-	  public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3){
-	        Integer max = a1;
-	        if (a2.compareTo(max) > 0)
-	            max = a2;
-	        if (a3.compareTo(max) > 0)
-	            max = a3;
-	        return max;
-	    }
+    /**
+     * Generic method to store values in array and return maximum value.
+     * We can store any size parameter
+     * @param elements : Stores the array value and compare it
+     * @return max : Maximum of three values
+     */
 
-	    /**
-	     * Method to compare three float values and get the maximum value.
-	     * @param f1 : First value to compare.
-	     * @param f2 : Second value to compare.
-	     * @param f3 : Third value to compare.
-	     * @return max : Maximum of three float
-	     */
+    public static <T extends Comparable<T>> T maxOfValues(T[] elements) {
+        Arrays.sort(elements);
+        int lenght = elements.length;
+        T max = elements[lenght - 1];
+        return max;
+    }
 
-	    public static Float maxOfFloat(Float f1, Float f2, Float f3) {
-	        Float max = f1;
-	        if (f2.compareTo(max) > 0)
-	            max = f2;
-	        if (f3.compareTo(max) > 0)
-	            max = f3;
-	        return max;
-	    }
+    public static void main(String[] args) {
+        System.out.println("Welcome To Find Maximun Problem Using Generics \n");
+        Integer[] intMax = { 10, 24, 68, 80, 42, 46, 12, 58 };
+        System.out.println("The maximum value between the three integer is : " + maxOfValues(intMax)+ "\n");
+        Float[] floatMax = { 1.4f, 87.3f, 81.8f, 40.2f, 4.6f };
+        System.out.println("The maximum value between the three float is : " + maxOfValues(floatMax)+ "\n");
+        String[] stringMax = { "ABC", "IJK", "PQR", "BCD", "MNO", "LMN", "RST", "XYZ" };
+        System.out.println("The maximum value between the three string is : " + maxOfValues(stringMax));
+    }
+} 
 
-	    /**
-	     * Method to compare three strings and get the maximum value.
-	     * @param s1 : First string to compare.
-	     * @param s2 : Second string to compare.
-	     * @param s3 : Third string to compare.
-	     * @return max : Maximum of three strings
-	     */
-
-	    public static String maxOfString(String s1, String s2, String s3) {
-	        String max = s1;
-	        if (s2.compareTo(max) > 0)
-	            max = s2;
-	        if (s3.compareTo(max) > 0)
-	            max = s3;
-	        return max;
-	    }
-
-	    public static void main(String[] args) {
-	        System.out.println("Welcome To Find Maximun Problem Using Generics \n");
-	        System.out.println("The maximum value between the three integer is : " + maxOfInteger(3, 8, 5)+ "\n");
-	        System.out.println("The maximum value between the three float is : " + maxOfFloat(1.5f, 3.56f, 5.87f) + "\n");
-	        System.out.println("The maximum value between the three string is : " + maxOfString("abc", "pqr", "xyz"));
-	    }
-	}
